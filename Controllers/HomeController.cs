6c5e474bf56e410458e37b72c7c0c5c2e93cff6c
friedcouch <S10203108@connect.np.edu.sg>
2021-04-22 16:09:01 +0800

    Practical 1 and 2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Web_S10203108.Models;
+
+namespace Web_S10203108.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult StaffLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // Email address converted to lowercase
+            string loginID = formData["txtLoginID"].ToString().ToLower();
+            string password = formData["txtPassword"].ToString();
+
+            if (loginID == "abc@npbook.com" && password == "pass1234")
+            {
+                // Redirect user to the "StaffMain" view through an action
+                return RedirectToAction("StaffMain");
+            }
+            else
+            {
+                // Redirect user back to the index view through an action
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult StaffMain()
+        {
+            return View();
+        }
+    };
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_S10203108.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
